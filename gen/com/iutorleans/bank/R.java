06f0ed46d9040913971ec8f98a571e6ce0110674
jinjin <wangxiao8808@hotmail.com>
2016-06-11 14:34:55 +0200

    creation des menus

--- gen/com/iutorleans/bank/R.java
+++ gen/com/iutorleans/bank/R.java
@@ -21,17 +21,34 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int add=0x7f020000;
+        public static final int charger=0x7f020001;
+        public static final int comptes=0x7f020002;
+        public static final int crediter=0x7f020003;
+        public static final int debiter=0x7f020004;
+        public static final int fichier=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int quitter=0x7f020007;
+        public static final int sauve=0x7f020008;
+        public static final int supprimer=0x7f020009;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
         public static final int bt_add=0x7f080000;
         public static final int bt_del=0x7f080001;
         public static final int bt_query=0x7f080003;
         public static final int bt_update=0x7f080002;
+        public static final int charger=0x7f080009;
+        public static final int comptes=0x7f08000b;
+        public static final int crediter=0x7f08000e;
+        public static final int creer=0x7f08000c;
+        public static final int debiter=0x7f08000d;
+        public static final int fichiers=0x7f080007;
         public static final int item_tv_nom=0x7f080005;
         public static final int item_tv_solde=0x7f080006;
         public static final int lv_comptes=0x7f080004;
+        public static final int quitter=0x7f08000a;
+        public static final int sauver=0x7f080008;
+        public static final int supprimer=0x7f08000f;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/iutorleans/bank/MainActivity.java
+++ src/com/iutorleans/bank/MainActivity.java
@@ -11,83 +11,110 @@ import android.app.Activity;
 import android.content.Context;
 //import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 import android.view.View.OnClickListener;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener {
 
-    private Context mContext;
-
+	private Context mContext;
 
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        mContext = this;
-        
-        //ListView lv_comptes = (ListView) findViewById(R.id.lv_comptes);
-        //ArrayList<ComptesBean> allComptes= ComptesUtils.getAllComptes();
-        
-        //ComptesAdapter comptesAdapter = new ComptesAdapter(mContext, allComptes);
-        //lv_comptes.setAdapter(comptesAdapter);
-        //MySqliteOpenHelper mySqliteOpenHelper = new MySqliteOpenHelper(mContext);
-       // SQLiteDatabase db = mySqliteOpenHelper.getReadableDatabase();
-        
-        findViewById(R.id.bt_add).setOnClickListener(this);
-        findViewById(R.id.bt_query).setOnClickListener(this);
-        
-    }
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
+		mContext = this;
+
+		// ListView lv_comptes = (ListView) findViewById(R.id.lv_comptes);
+		// ArrayList<ComptesBean> allComptes= ComptesUtils.getAllComptes();
+
+		// ComptesAdapter comptesAdapter = new ComptesAdapter(mContext,
+		// allComptes);
+		// lv_comptes.setAdapter(comptesAdapter);
+		// MySqliteOpenHelper mySqliteOpenHelper = new
+		// MySqliteOpenHelper(mContext);
+		// SQLiteDatabase db = mySqliteOpenHelper.getReadableDatabase();
+
+		findViewById(R.id.bt_add).setOnClickListener(this);
+		findViewById(R.id.bt_query).setOnClickListener(this);
+
+	}
 
 	@Override
 	public void onClick(View v) {
-		
+
 		ComptesDao comptesDao = new ComptesDao(mContext);
-		
-		switch(v.getId()){
-		
+
+		switch (v.getId()) {
+
 		case R.id.bt_add:
-			
+
 			ComptesBean comptesBean = new ComptesBean();
-			comptesBean.nom="kiki";
-			comptesBean.solde=7200;
-			
+			comptesBean.nom = "kiki";
+			comptesBean.solde = 7200;
+
 			comptesDao.add(comptesBean);
-			
+
 			ComptesBean comptesBean1 = new ComptesBean();
-			comptesBean1.nom="wowo";
-			comptesBean1.solde=120000;
-			
+			comptesBean1.nom = "wowo";
+			comptesBean1.solde = 120000;
+
 			comptesDao.add(comptesBean1);
-			
-			
-			
-			
+
 			break;
-			
+
 		case R.id.bt_query:
-			
-			
+
 			ListView lv_comptes = (ListView) findViewById(R.id.lv_comptes);
-	        ArrayList<ComptesBean> allComptes= comptesDao.query();
-	        
-	        ComptesAdapter comptesAdapter = new ComptesAdapter(mContext, allComptes);
-	        lv_comptes.setAdapter(comptesAdapter);
-			
+			ArrayList<ComptesBean> allComptes = comptesDao.query();
+
+			ComptesAdapter comptesAdapter = new ComptesAdapter(mContext,
+					allComptes);
+			lv_comptes.setAdapter(comptesAdapter);
+
 			break;
-			
-			default:
-				break;
-			
-		
+
+		default:
+			break;
+
 		}
-		
+
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		
+		getMenuInflater().inflate(R.menu.main, menu);
 
-    
-    
+		return true;
+
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		switch (item.getItemId()) {
+		case R.id.sauver:
+			
+			Toast.makeText(this, "sauver", 1).show();
+
+			break;
+
+		case R.id.creer:
+			
+			
+			Toast.makeText(this, "creer", 1).show();
+
+			break;
+
+		default:
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
 }
